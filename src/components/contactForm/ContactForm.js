a99f410dc88d5a04d03360ab61e3cfdc8d3e9bdc
meredithdean <meredithdvhamilton@gmail.com>
2022-09-10 18:01:57 +0100

    implement ContactForm component to render form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,39 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input 
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+        placeholder="Contact Name"/>
+      </label>
+      <br />
+      <label>
+        <input 
+        type="tel"
+        name="phone"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+        required
+        pattern="/^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/"
+        placeholder="Contact Phone Number +44 or (XXXX)"/>
+      </label>
+      <br />
+      <label>
+        <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Contact Email"/>
+      </label>
+      <br />
+      <input type="submit" value="Add Contact"/>
+    </form>
   );
 };
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -50,11 +50,24 @@ export const ContactsPage = ({ contacts, addContact }) => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+      <h2>
+          Add Contact
+          {duplicate ? " - Name Already Exists" : ""}
+        </h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList tiles={contacts} />
       </section>
     </div>
   );
