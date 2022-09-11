89cfe08275f97f3022f6263c688e3a7f522c6272
meredithdean <meredithdvhamilton@gmail.com>
2022-09-11 14:11:09 +0100

    implement AppointmentForm to render a web form

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from "../contactPicker/ContactPicker";
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +21,47 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <form onsubmit={handleSubmit}>
+      <label>
+        <input 
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder="Appointment For" />
+      </label>
+      <br />
+      <label>
+        <ContactPicker 
+          name="contact"
+          value={contact}
+          onChange={(e) => setContact(e.target.value)}
+          contacts={contacts}
+          required
+          placeholder="Appointment With" />
+      </label>
+      <br />
+      <label>
+        <input 
+          type="date"
+          name="date"
+          value={date} 
+          onChange={(e) => setDate(e.target.value)}
+          min={getTodayString()}
+          required />
+      </label>
+      <br />
+      <label>
+        <input 
+          type="time"
+          name="time"
+          value={time} 
+          onChange={(e) => setTime(e.target.value)}
+          required />
+      </label>
+      <br />
+      <input type="submit" value="add appointment"/>
+    </form>
   );
 };
